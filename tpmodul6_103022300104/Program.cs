8278ca3580fd81cf2a5719e172b06a3d24781c73
rizaptrarz <rizaputra501@gmail.com>
2025-03-23 21:42:48 +0700

    Menambahkan class SayaTubeVideo

--- tpmodul6_103022300104/Program.cs
+++ tpmodul6_103022300104/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo videos = new SayaTubeVideo("Tutorial Design By Contract - Riza Muhammad Putra");
+
+        for (int i = 0; i < 3; i++)
+        {
+            videos.IncreasePlayCount(10000000);
+            videos.PrintVideoDetails();
+        }
+
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Riza Muhammad Putra");
+            video.IncreasePlayCount(10000001);
+            video.PrintVideoDetails();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ tpmodul6_103022300104/SayaTubeVideo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (string.IsNullOrEmpty(title) || title.Length > 100)
+        {
+            throw new ArgumentException("Judul video tidak boleh kosong dan maksimal 100 karakter.");
+        }
+
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count > 10000000)
+        {
+            throw new ArgumentException("Penambahan play count maksimal 10.000.000 per panggilan.");
+        }
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Overflow terjadi pada penambahan play count.");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
